fab20acea289373e49478118470da513d52e0404
Robert Weilbaecher <robertgweilbaecher@gmail.com>
2016-11-15 16:00:40 -0600

    fixed env vars

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,11 +5,11 @@ const env = runtimeEnv();
 
 
 const config = {
-    apiKey: process.env.FBAPIKEY1,
-    authDomain: process.env.FBAUTHDOMAIN1,
-    databaseURL: process.env.FBDATABASEURL1,
-    storageBucket: process.env.FBSTORAGEBUCKET1,
-    messagingSenderId: process.env.FBMESSAGINGSENDERID1
+    apiKey: process.env.REACT_APP_FBAPIKEY1,
+    authDomain: process.env.REACT_APP_FBAUTHDOMAIN1,
+    databaseURL: process.env.REACT_APP_FBDATABASEURL1,
+    storageBucket: process.env.REACT_APP_FBSTORAGEBUCKET1,
+    messagingSenderId: process.env.REACT_APP_FBMESSAGINGSENDERID1
   };
 
 firebase.initializeApp(config);
