d086eb6264f283883697558af7f2abf70032ba61
Robert Weilbaecher <robertgweilbaecher@gmail.com>
2016-11-15 14:12:15 -0600

    changing sign in

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -8,9 +8,10 @@ export default class LoginButton extends Component{
 
   _handleClick(e){
     e.preventDefault();
-    let provider = new
-    this.props.firebase.auth.GoogleAuthProvider();
-    this.props.firebase.auth().signInWithPopup(provider)
+    let provider = new this.props.firebase.auth().signInWithEmailAndPassword(email, password);
+    // let provider = new
+    // this.props.firebase.auth.GoogleAuthProvider();
+    this.props.firebase.auth().signIn(provider)
   }
 
   render(){
